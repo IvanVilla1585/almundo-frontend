e4a3caae0878e26c13a2597a435af7b8b59bf264
Ivan Villa <trezeguet55@gmail.com>
2018-01-13 21:32:25 -0500

    <components hotels> create hotel component

--- src/app/components/hotels/hotels.component.ts
+++ src/app/components/hotels/hotels.component.ts
@@ -1,31 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {HotelService} from '../../services/hotel/hotel.service';
 
 @Component({
   selector: 'app-hotels',
   templateUrl: './hotels.component.html',
-  styleUrls: ['./hotels.component.css'],
-  providers: [HotelService]
+  styleUrls: ['./hotels.component.css']
 })
 export class HotelsComponent implements OnInit {
-  public hotels: Array<object>;
 
-  constructor(private _hotelService: HotelService) {
-    this.hotels = [];
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.getDataHotels();
-  }
-  getDataHotels() {
-    this._hotelService.findHotels('').subscribe(
-      (res: any) => {
-        this.hotels = res;
-      },
-      (err) => {
-        console.log('err', err);
-      }
-    );
   }
 
 }
