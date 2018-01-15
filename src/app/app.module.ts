7af3c483c527208e65a43941979de359502c04c7
Ivan Villa <trezeguet55@gmail.com>
2018-01-15 14:43:28 -0500

    <app app..module.ts>
    configuration components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import {HotelsComponent} from './components/hotels/hotels.component';
+import { HeaderComponent } from './common/header/header.component';
+import { IconStarComponent } from './common/icon-star/icon-star.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {HotelsRoutingModule} from './components/hotels/hotels-routing.module';
+import { IconSearchComponent } from './common/icon-search/icon-search.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HotelsComponent,
+    IconStarComponent,
+    IconSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HotelsRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
